91743a4ab90fa045eb2d01b952e4cd8159c37f34
vitalinosilva <vitalinosilva@gmail.com>
2026-01-15 16:53:30 +0000

    Fixed linting errors

--- libs/matmatrix-core/src/studentManagement/__tests__/student-management-service.spec.ts
+++ libs/matmatrix-core/src/studentManagement/__tests__/student-management-service.spec.ts
@@ -3,19 +3,18 @@ import {describe, expect, it} from "vitest";
 import type { StudentManagementDriverPorts } from '../ports/driver/student-management-driver.ports';
 import type { StudentManagementReaderDrivenPorts } from '../ports/driven/student-management-reader-driven.ports';
 import type { StudentManagementWriterDrivenPorts } from '../ports/driven/student-management-writer-driven.ports';
-import type { Student } from '../core/entities/student.entity';
 import { StudentManagementService } from '../student-management.service';
 
 
 describe("Student Management Service Tests", () => {
 
   const fakeReader: StudentManagementReaderDrivenPorts = {
-    get: async (id?:string):Promise<Student[] | Student | null> => null
+    get: () => Promise.resolve(null)
   };
 
   const fakeWriter: StudentManagementWriterDrivenPorts = {
-    write: async (student: Student):Promise<void> => {},
-    erase: async (studentId: string):Promise<void> => {}
+    write: () => Promise.resolve(),
+    erase: () => Promise.resolve()
   };
 
   const studentManagementService: StudentManagementDriverPorts = StudentManagementService(
